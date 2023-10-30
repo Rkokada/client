52ad1f871697aa5b0184bdf14bbc98b70b4ffcc0
Ramond <rocha@email.com>
2023-10-30 17:01:22 -0300

    first Commit

---
+++ src/components/Prices.jsx
@@ -0,0 +1,32 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "R$0 a 19",
+    array: [0, 19],
+  },
+  {
+    _id: 1,
+    name: "R$20 a 39",
+    array: [20, 39],
+  },
+  {
+    _id: 2,
+    name: "R$40 a 59",
+    array: [40, 59],
+  },
+  {
+    _id: 3,
+    name: "R$60 a 79",
+    array: [60, 79],
+  },
+  {
+    _id: 4,
+    name: "R$80 a 99",
+    array: [80, 99],
+  },
+  {
+    _id: 4,
+    name: "R$100 ou mais",
+    array: [100, 9999],
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "antd/dist/reset.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthProvider } from "./context/auth";
+import { CartProvider } from "./context/cart";
+import { SearchProvider } from "./context/search";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider>
+    <SearchProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
+    </SearchProvider>
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
